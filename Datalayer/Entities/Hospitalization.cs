3e92cd583bcbef4d5db31be7f8f02751086f8f9c
gamingAmee <amalie2410@gmail.com>
2022-08-19 09:30:53 +0200

    EditBooking works

---
+++ Datalayer/Entities/Hospitalization.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datalayer.Entities
+{
+	public class Hospitalization
+	{
+		public int HospitalizationID { get; set; }
+		public string Reason { get; set; }
+		public bool IsDischarge { get; set; } = false;
+		public int PatientId { get; set; }
+		public int HospitalId { get; set; }
+	}
+}
